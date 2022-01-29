dc1195f62d279da39f391e4b7f45786322843b2f
Marcelus Trojahn <mtrojahn@gmail.com>
2022-01-29 14:59:26 -0300

    docker support

--- src/py/webservices/etl_api/app.py
+++ src/py/webservices/etl_api/app.py
@@ -1,14 +1,20 @@
 import uvicorn as uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 from libs.converters.etl import convert_to_string
 
 app = FastAPI()
 
 
+class DefaultResult(BaseModel):
+    message: str
+
+
 @app.get("/")
-def get_string(number: int):
-    return {"result": convert_to_string(number)}
+def get_string(number: int) -> DefaultResult:
+    return DefaultResult(message=convert_to_string(number))
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0")
